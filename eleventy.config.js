4509113158528d173c367a93e4f06ebd65283963
JOSE MANUEL UGALDE BORBOLLA <jmugaldeb@gmail.com>
2023-03-12 14:36:05 -0600

    ChangedNav

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,14 @@
+const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
-    watch: ['_site/**/*.css'],
+    watch: ['_site/*.css'],
   });
+
+  // human readable date
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).setLocale('es-MX').toLocaleString(DateTime.DATE_FULL);
+  });
+
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
   eleventyConfig.addPassthroughCopy("./src/static/img");
