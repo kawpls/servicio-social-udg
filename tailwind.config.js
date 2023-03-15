fdf39aed4ae0e62e3f7acfd608d6284dc96f521f
JOSE MANUEL UGALDE BORBOLLA <jmugaldeb@gmail.com>
2023-03-15 08:35:13 -0600

    UI Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,31 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        basetheme: {
+
+          "primary": "#823f80",
+
+          "secondary": "#dfc1ce",
+
+          "accent": "#c4b5fd",
+
+          "neutral": "#44403c",
+
+          "base-100": "#f5f5f4",
+
+          "info": "#bae6fd",
+
+          "success": "#6ee7b7",
+
+          "warning": "#fecaca",
+
+          "error": "#fda4af",
+        },
+      },
+    ],
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
