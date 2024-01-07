5bf9b15f358b9bd2d70a80dd465525aeb7d85a0f
Manuel Ugalde <jmugaldeb@gmail.com>
2024-01-06 23:44:28 -0600

    Update eleventy.config.js, package.json, and admin/config.yml

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,22 @@
 const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
-    watch: ['_site/*.css'],
+    watch: ["_site/*.css"],
   });
 
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).setLocale('es-MX').toLocaleString(DateTime.DATE_FULL);
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).setLocale("es-MX").toLocaleString(DateTime.DATE_FULL);
   });
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
-  eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("./src/static");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/email templates");
   return {
     dir: {
-      input: 'src',
+      input: "src",
     },
   };
 };
