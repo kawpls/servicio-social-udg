d9eebd23212aa3decc00b4ce6b8b2948edaa1f71
Manuel Ugalde <jmugaldeb@gmail.com>
2024-01-17 09:24:10 -0600

    Add theme change script and fix translation attribute

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,njk}'],
+  content: ["./src/**/*.{html,njk}"],
   theme: {
     extend: {
       fontFamily: {
-        'oswald': ['Oswald', "sans-serif"]
+        oswald: ["Oswald", "sans-serif"],
       },
     },
   },
@@ -12,26 +12,57 @@ module.exports = {
     themes: [
       {
         basetheme: {
+          primary: "#823f80",
 
-          "primary": "#823f80",
+          secondary: "#dfc1ce",
 
-          "secondary": "#dfc1ce",
+          accent: "#c4b5fd",
 
-          "accent": "#c4b5fd",
-
-          "neutral": "#44403c",
+          neutral: "#44403c",
 
           "base-100": "#f5f5f4",
 
-          "info": "#bae6fd",
+          info: "#bae6fd",
 
-          "success": "#6ee7b7",
+          success: "#6ee7b7",
 
-          "warning": "#fecaca",
+          warning: "#fecaca",
 
-          "error": "#fda4af",
+          error: "#fda4af",
         },
       },
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
     ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
