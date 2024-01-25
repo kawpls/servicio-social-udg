448a88caf6b578ee649faf4c18a96c33ce23d6ee
Manuel Ugalde <jmugaldeb@gmail.com>
2024-01-25 08:18:27 -0600

    Add passthrough copy for sitemap.xml

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
   eleventyConfig.addPassthroughCopy("./src/static");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
   return {
     dir: {
       input: "src",
